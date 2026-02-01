88f0f8dd1bdcb03f78a5872d33e28ab7ae7bb39f
Madhesh-2907 <c.madheshkumar2006@gmail.com>
2026-02-01 22:03:51 +0530

    Created Expense Tracker

---
+++ script.js
@@ -0,0 +1,35 @@
+let total = 0;
+
+function addExpense() {
+    let name = document.getElementById("name").value;
+    let amount = document.getElementById("amount").value;
+    let category = document.getElementById("category").value;
+
+    if (name === "" || amount === "" || category === "") {
+        alert("Please fill all fields");
+        return;
+    }
+
+    amount = Number(amount);
+
+    let li = document.createElement("li");
+    li.innerHTML = `
+        ${name} (${category}) - ₹${amount}
+        <button onclick="removeExpense(this, ${amount})">X</button>
+    `;
+
+    document.getElementById("expenseList").appendChild(li);
+
+    total += amount;
+    document.getElementById("total").innerText = total;
+
+    document.getElementById("name").value = "";
+    document.getElementById("amount").value = "";
+    document.getElementById("category").value = "";
+}
+
+function removeExpense(button, amount) {
+    button.parentElement.remove();
+    total -= amount;
+    document.getElementById("total").innerText = total;
+}
